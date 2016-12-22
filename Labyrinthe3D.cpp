66dc503357dac6b1c1b9119c4303207065473124
bzetR <robinbizet@gmail.com>
2016-12-22 19:04:50 +0100

    jai ajoute labyrinthe3D.cpp

---
+++ Labyrinthe3D.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+#include "Labyrinthe3D.h"
+
+/**
+ * @file Labyrinthe3D.cpp
+ * @author Robin BIZET
+ * @version 1 20/12/2016
+ * @brief Projet - Spécialisation des fonctions correspondant au Labyrinthe3D
+ */
+
+void initialiser(Labyrinthe3D& l, unsigned int dimX,
+                 unsigned int dimY, unsigned int dimZ);
+
+
+
+void detruire(Labyrinthe3D& l);
+
