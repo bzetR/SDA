e0eadca7e0d92ef347c1404ee5356c0b8701c83a
bzetR <robinbizet@gmail.com>
2016-12-24 04:33:50 +0100

    Ajout du main avec la boucle

---
+++ main.cpp
@@ -0,0 +1,19 @@
+nclude <iostream>
+#include <cassert>
+using namespace std;
+
+#include "Labyrinthe3D.h"
+
+int main(int argc, const char * argv[]) {
+    Labyrinthe3D lab;
+    cin >> lab.t3D.dimX >> lab.t3D.dimY >> lab.t3D.dimZ;
+    for (unsigned int i = 0; i < lab.t3D.dimZ; ++i){
+        for (unsigned int j = 0; j <lab.t3D.dimY; ++j){
+            for (unsigned int u = 0; u < lab.t3D.dimX; ++u){
+            	    
+            }
+        }
+    }
+    return 0;
+}
+
